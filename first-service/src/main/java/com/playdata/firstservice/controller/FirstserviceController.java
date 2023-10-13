2116fca2e1cf00705871b0cd25108d21505aa90b
dev.rary@gmail.com <dev.rary@gmail.com>
2023-10-13 14:04:27 +0900

    10월 13일 4교시, yml 필터 등록

--- first-service/src/main/java/com/playdata/firstservice/controller/FirstserviceController.java
+++ first-service/src/main/java/com/playdata/firstservice/controller/FirstserviceController.java
@@ -1,9 +1,6 @@
 package com.playdata.firstservice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("first-service") // gateway서버의 predicates의 식별 주소를 추가해야 합니다.
@@ -12,5 +9,10 @@ public class FirstserviceController {
     @RequestMapping(value = "hello", method = RequestMethod.GET)
     public String hello(){
         return "Hello, First-service!";
+    }
+
+    @GetMapping("header-check")
+    public String headerCheck(@RequestHeader("fsreqhyml") String header){
+        return header;
     }
 }
